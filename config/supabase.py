08b5dac5f42ba542a2b324b8f8737128533a6930
cairomedeiros <cairomedeiros13@gmail.com>
2026-02-12 17:37:49 -0300

    add supabase client admin

--- config/supabase.py
+++ config/supabase.py
@@ -6,5 +6,18 @@ supabase: Client = create_client(
     settings.SUPABASE_PUBLISHABLE_KEY
 )
 
+supabase_admin: Client | None = None
+
+if settings.SUPABASE_SECRET_KEY:
+    supabase_admin = create_client(
+        settings.SUPABASE_URL,
+        settings.SUPABASE_SECRET_KEY
+    )
+
 def get_db():
     return supabase
+
+def get_supabase_admin():
+    if not supabase_admin:
+        raise RuntimeError("Secret key not configured.")
+    return supabase_admin

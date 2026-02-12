00fe3356143d2f8f3e13ed527b4d42c21715d5cc
cairomedeiros <cairomedeiros13@gmail.com>
2026-02-12 17:38:33 -0300

    add dependencies to validate the current user

--- auth/dependencies.py
+++ auth/dependencies.py
@@ -1,14 +1,21 @@
 
 from fastapi import HTTPException, status, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from ..config import supabase
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+bearer_scheme = HTTPBearer()
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        supabase_db = supabase.get_db()
-        user = supabase_db.auth.get_user(token)
-        return user
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    token = credentials.credentials
+    supabase_db = supabase.get_db()
+    user_response = supabase_db.auth.get_user(jwt=token)
+
+    if user_response.user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+        )
+    response = {"user": user_response.user, "access_token": token}
+    return response

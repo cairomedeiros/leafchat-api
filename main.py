80ac27e917b102d29a1db813b759485bc82a9bca
cairomedeiros <cairomedeiros13@gmail.com>
2026-02-15 15:49:26 -0300

    add tags and prefix to plant and user router

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from .routers import user
+from .routers import user, plant
 
 app = FastAPI()
-app.include_router(user.router)
+app.include_router(user.router, prefix="/user", tags=["user"])
+app.include_router(plant.router, prefix="/plant", tags=["plant"])
 
 @app.get("/")
 async def root():

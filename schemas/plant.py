12c1ee0dcb0d62dd877cf01fa92021e95a9cce13
cairomedeiros <cairomedeiros13@gmail.com>
2026-02-21 08:51:01 -0300

    att plant schema

--- schemas/plant.py
+++ schemas/plant.py
@@ -1,4 +1,8 @@
 from pydantic import BaseModel
 
-class PlantBase(BaseModel):
+class PlantCreate(BaseModel):
     name: str
+    description: str | None = None
+
+class PlantUpdate(PlantCreate):
+    pass
